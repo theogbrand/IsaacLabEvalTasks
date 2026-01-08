a5a05b0c46cfd89282c4c9d062197885907f559b
ob1 <ongjjbrandon@gmail.com>
2026-01-08 10:29:50 +0000

    change back to ob1_ws

--- ob1_setup_server.sh
+++ ob1_setup_server.sh
@@ -4,17 +4,17 @@ git config --global user.email ongjjbrandon@gmail.com
 apt update
 apt install ffmpeg libsm6 libxext6 tmux
 
-mkdir -p /workspace/miniconda3
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O /workspace/miniconda3/miniconda.sh
-bash /workspace/miniconda3/miniconda.sh -b -u -p /workspace/miniconda3
-rm /workspace/miniconda3/miniconda.sh
+mkdir -p /ob1_ws/miniconda3
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O /ob1_ws/miniconda3/miniconda.sh
+bash /ob1_ws/miniconda3/miniconda.sh -b -u -p /ob1_ws/miniconda3
+rm /ob1_ws/miniconda3/miniconda.sh
 
-source /workspace/miniconda3/bin/activate
+source /ob1_ws/miniconda3/bin/activate
 
 conda init --all
 
-conda config --add envs_dirs /workspace/conda/envs
-conda config --add pkgs_dirs /workspace/conda/pkgs
+conda config --add envs_dirs /ob1_ws/conda/envs
+conda config --add pkgs_dirs /ob1_ws/conda/pkgs
 
 conda tos accept --override-channels --channel https://repo.anaconda.com/pkgs/main
 conda tos accept --override-channels --channel https://repo.anaconda.com/pkgs/r
